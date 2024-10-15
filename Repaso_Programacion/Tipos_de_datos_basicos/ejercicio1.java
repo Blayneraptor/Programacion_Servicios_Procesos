88702813c7ad20d306db255aba54ce7c7a9d5d2a
Blayneraptor <cousoworks@gmail.com>
2024-10-15 20:03:30 +0200

    Añadiendo ejercicio 1

---
+++ Repaso_Programacion/Tipos_de_datos_basicos/ejercicio1.java
@@ -0,0 +1,26 @@
+package Repaso_Programacion.Tipos_de_datos_basicos;
+
+/*### 📝 Ejercicio 1:
+- Crea un programa que solicite al usuario introducir su edad y su altura.
+- La edad debe almacenarse en una variable de tipo `int`, y la altura en una variable de tipo `double`.
+- Imprime ambos valores con un mensaje apropiado.*/
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+
+        int numero = 10;
+        double decimal = 3.14;
+        char caracter = 'A';
+        boolean verdadero = true;
+        String texto = "Hola Mundo";
+
+        System.out.println(numero);
+        System.out.println(decimal);
+        System.out.println(caracter);
+        System.out.println(verdadero);
+        System.out.println(texto);
+
+    }
+        
+}
