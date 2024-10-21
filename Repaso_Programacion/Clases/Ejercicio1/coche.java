c21d1be85749286d2a86e1f825c692e4f1619882
Blayneraptor <cousoworks@gmail.com>
2024-10-21 19:25:53 +0200

    Añadiendo clase coche

---
+++ Repaso_Programacion/Clases/Ejercicio1/coche.java
@@ -0,0 +1,29 @@
+package Repaso_Programacion.Clases.Ejercicio1;
+
+/* Ejercicio 1:
+Define una clase Coche que tenga como atributos el modelo y la velocidadMaxima.
+Añade un método mostrarCaracteristicas() que imprima los datos del coche.
+Crea una instancia de esta clase en el main y llama al método mostrarCaracteristicas().
+
+Autor: Jose P. Couso Pérez*/
+
+// Definición de la clase Coche
+class Coche {
+    // Atributos de la clase Coche
+    String modelo;
+    int velocidadMaxima;
+
+    // Constructor de la clase Coche
+    public Coche(String modelo, int velocidadMaxima) {
+        this.modelo = modelo;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    // Método para mostrar las características del coche
+    public void mostrarCaracteristicas() {
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Velocidad Máxima: " + velocidadMaxima + " km/h");
+    }
+}
+
+
---
+++ Repaso_Programacion/Clases/Ejercicio1/maincoche.java
@@ -0,0 +1,11 @@
+package Repaso_Programacion.Clases.Ejercicio1;
+
+public class maincoche {
+    public static void main(String[] args) {
+        // Crear una instancia de la clase Coche
+        Coche miCoche = new Coche("Toyota Supra", 280);
+
+        // Llamar al método mostrarCaracteristicas()
+        miCoche.mostrarCaracteristicas();
+    }
+}
