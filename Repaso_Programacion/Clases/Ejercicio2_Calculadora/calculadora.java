e6f0d49cced119b623450aff46024630e511cb50
Blayneraptor <cousoworks@gmail.com>
2024-10-21 20:00:00 +0200

    Añadiendo carpeta Encapsulación

--- Repaso_Programacion/Clases/Ejercicio2_Calculadora/calculadora.java
+++ Repaso_Programacion/Clases/Ejercicio2_Calculadora/calculadora.java
@@ -1,5 +1,12 @@
 package Repaso_Programacion.Clases.Ejercicio2_Calculadora;
 
+/* Ejercicio 1:
+ Crea una clase Calculadora que contenga métodos para sumar, restar,
+ multiplicar y dividir dos números pasados como parámetros. Luego, crea
+ una instancia de esta clase en el main y utiliza cada uno de los métodos.
+
+Autor: Jose P. Couso Pérez*/
+
 // Definición de la clase Calculadora
 class Calculadora {
 
---
+++ Repaso_Programacion/Encapsulacion_visibilidad/Ejercicio1_CuentaBancaria/cuentabancaria.java
@@ -0,0 +1,12 @@
+package Repaso_Programacion.Encapsulacion_visibilidad.Ejercicio1_CuentaBancaria;
+
+/*Diseña una clase CuentaBancaria que encapsule el comportamiento de una cuenta.
+Debe tener los atributos privados titular y saldo, y los
+métodos públicos para depositar() y retirar() dinero, así como getSaldo() para consultar el saldo
+Implementa también un método mostrarInformacion() para imprimir el titular y el saldo de la cuenta.
+
+Autor: Jose P. Couso Pérez*/
+
+public class cuentabancaria {
+    
+}
