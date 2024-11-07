e135db40cc84a14168904476a1d2dc2d7bf4714e
Blayneraptor <cousoworks@gmail.com>
2024-11-07 11:42:55 +0100

    Ejercicio cuenta bancaria

--- Repaso_Programacion/Encapsulacion_visibilidad/Ejercicio1_CuentaBancaria/cuentabancaria.java
+++ Repaso_Programacion/Encapsulacion_visibilidad/Ejercicio1_CuentaBancaria/cuentabancaria.java
@@ -8,5 +8,46 @@ Implementa también un método mostrarInformacion() para imprimir el titular y e
 Autor: Jose P. Couso Pérez*/
 
 public class cuentabancaria {
-    
+    // Atributos privados
+    private String titular;
+    private double saldo;
+
+    // Constructor
+    public cuentabancaria(String titular, double saldoInicial) {
+        this.titular = titular;
+        this.saldo = saldoInicial;
+    }
+
+    // Método para depositar dinero
+    public void depositar(double cantidad) {
+        if (cantidad > 0) {
+            saldo += cantidad;
+            System.out.println("Se han depositado: " + cantidad + "€. Nuevo saldo: " + saldo + "€.");
+        } else {
+            System.out.println("La cantidad a depositar debe ser positiva.");
+        }
+    }
+
+    // Método para retirar dinero
+    public void retirar(double cantidad) {
+        if (cantidad > 0 && cantidad <= saldo) {
+            saldo -= cantidad;
+            System.out.println("Se han retirado: " + cantidad + "€. Nuevo saldo: " + saldo + "€.");
+        } else if (cantidad > saldo) {
+            System.out.println("Saldo insuficiente.");
+        } else {
+            System.out.println("La cantidad a retirar debe ser positiva.");
+        }
+    }
+
+    // Método para obtener el saldo actual
+    public double getSaldo() {
+        return saldo;
+    }
+
+    // Método para mostrar la información de la cuenta
+    public void mostrarInformacion() {
+        System.out.println("Titular: " + titular);
+        System.out.println("Saldo actual: " + saldo + "€.");
+    }
 }
